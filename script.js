4327e734d19547262d675e60fac69d3bea7cddde
TechSubham353 <ranasubham129@gmail.com>
2026-04-01 22:11:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        let value = e.target.innerHTML;
+
+        if (value === "AC") {
+            string = "";
+            input.value = string;
+        }
+        else if (value === "DEL") {
+            string = string.slice(0, -1);
+            input.value = string;
+        }
+        else if (value === "=") {
+            try {
+                string = eval(string);
+                input.value = string;
+            } catch {
+                input.value = "Error";
+                string = "";
+            }
+        }
+        else {
+            string += value;
+            input.value = string;
+        }
+    });
+});
